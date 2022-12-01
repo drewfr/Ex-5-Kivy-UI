c69300370d15cbcb825f0fb3427a41a3a28d982c
Student <andrewhamers@dpengineering.org>
2022-12-01 13:34:26 -0800

    Finished UI for first Lesson

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
+import pygame
 
-#os.environ['DISPLAY'] = ":0.0"
-#os.environ['KIVY_WINDOW'] = 'egl_rpi'
+# os.environ['DISPLAY'] = ":0.0"
+# os.environ['KIVY_WINDOW'] = 'egl_rpi'
 
 from kivy.app import App
+from kivy.properties import StringProperty
+import pickle
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
+from kivy.animation import Animation
+from kivy.clock import Clock
 
 from pidev.MixPanel import MixPanel
 from pidev.kivy.PassCodeScreen import PassCodeScreen
@@ -14,8 +20,13 @@ from pidev.kivy.PauseScreen import PauseScreen
 from pidev.kivy import DPEAButton
 from pidev.kivy import ImageButton
 from pidev.kivy.selfupdatinglabel import SelfUpdatingLabel
+from pidev.Joystick import Joystick
+from pidev.stepper import stepper
 
 from datetime import datetime
+
+pygame.init()
+joy = Joystick(number=0, ssh_deploy=True)
 
 time = datetime
 
@@ -24,6 +35,8 @@ MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
 
 SCREEN_MANAGER = ScreenManager()
 MAIN_SCREEN_NAME = 'main'
+SECOND_WINDOW_NAME = 'second'
+THIRD_WINDOW_NAME = 'third'
 ADMIN_SCREEN_NAME = 'admin'
 
 
@@ -47,6 +60,110 @@ class MainScreen(Screen):
     """
     Class to handle the main screen and its associated touch events
     """
+    counter = StringProperty("0")
+    num = 0
+    count = 0
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_interval(self.change_pos_label, 0.2)
+
+    def change_pos_label(self, dt):
+        self.lb2.text = "x: " + str(joy.get_axis('x')) + "    y: " + str(joy.get_axis('y'))
+
+    def animate_it(self, widget, *args):
+        animate = Animation(
+            background_color=(0, 0, 1, 1))
+
+        for x in range(20):
+            animate += Animation(
+                size_hint=(.3, .3))
+
+            animate += Animation(size_hint=(.2, .2))
+
+        animate.start(widget)
+
+    def move_to_second(self):
+        SCREEN_MANAGER.current = "second"
+
+    def move_to_third(self):
+        SCREEN_MANAGER.current = "third"
+
+    def turn_on(self):
+        self.ids.btn2.text = "on"
+
+    def turn_off(self):
+        self.ids.btn2.text = "off"
+
+    def add_one(self):
+        self.num += 1
+        self.counter = str(self.num)
+
+    def change_label(self):
+        self.count += 1
+        if self.count % 2:
+            self.lb1.text = "Motor on"
+        else:
+            self.lb1.text = "Motor off"
+
+
+class SecondWindow(Screen):
+    counter = StringProperty("0")
+    num = 0
+    count = 0
+
+    def go_home(self):
+        SCREEN_MANAGER.current = 'main'
+
+    def turn_on(self):
+        self.ids.btn2.text = "on"
+
+    def turn_off(self):
+        self.ids.btn2.text = "off"
+
+    def add_one(self):
+        self.num += 1
+        self.counter = str(self.num)
+
+    def change_label(self):
+        self.count += 1
+        if self.count % 2:
+            self.lb1.text = "Motor on"
+        else:
+            self.lb1.text = "Motor off"
+
+    def sliderfunction(self, *args):
+        # print(*args)
+        self.ids.labelvalue.text = str(int(args[1]))
+
+
+class ThirdWindow(Screen):
+
+    def go_home(self):
+        SCREEN_MANAGER.current = "main"
+
+    def click(self):
+        self.ids.my_image.source = 'images2.jpeg'
+
+    def off_click(self):
+        self.ids.my_image.source = 'images.jpeg'
+        self.go_home()
+
+    def animate_it(self, widget, *args):
+        animate = Animation(
+            background_color=(0, 0, 1, 1))
+
+        for x in range(20):
+            animate += Animation(opacity=1,
+                                 size_hint=(.3, .3))
+
+            animate += Animation(size_hint=(.2, .2))
+
+        animate.start(widget)
+
+    def anima(self, widget, *args):
+        anim = Animation(x=50) + Animation(size=(80, 80), duration=2.)
+        anim.start(widget)
 
     def pressed(self):
         """
@@ -77,8 +194,10 @@ class AdminScreen(Screen):
         """
         Builder.load_file('AdminScreen.kv')
 
-        PassCodeScreen.set_admin_events_screen(ADMIN_SCREEN_NAME)  # Specify screen name to transition to after correct password
-        PassCodeScreen.set_transition_back_screen(MAIN_SCREEN_NAME)  # set screen name to transition to if "Back to Game is pressed"
+        PassCodeScreen.set_admin_events_screen(
+            ADMIN_SCREEN_NAME)  # Specify screen name to transition to after correct password
+        PassCodeScreen.set_transition_back_screen(
+            MAIN_SCREEN_NAME)  # set screen name to transition to if "Back to Game is pressed"
 
         super(AdminScreen, self).__init__(**kwargs)
 
@@ -110,9 +229,10 @@ class AdminScreen(Screen):
 """
 Widget additions
 """
-
 Builder.load_file('main.kv')
 SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(SecondWindow(name=SECOND_WINDOW_NAME))
+SCREEN_MANAGER.add_widget(ThirdWindow(name=THIRD_WINDOW_NAME))
 SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
 SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
 SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
